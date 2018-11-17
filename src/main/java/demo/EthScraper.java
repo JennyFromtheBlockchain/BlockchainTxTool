e5a0360aedcde022913f9f2d3c0267678d38c15a
Chris P <jennyfromtheblockchain@outlook.com>
2018-11-17 11:42:27 +0000

    Initial commit.

---
+++ src/main/java/demo/EthScraper.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class EthScraper {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
